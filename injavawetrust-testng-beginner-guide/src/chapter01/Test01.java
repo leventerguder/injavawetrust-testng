b18147652ef143880a67334147805a486d405fd1
leventerguder <erguder.levent@gmail.com>
2019-05-19 12:54:06 +0300

    chapter 01

---
+++ injavawetrust-testng-beginner-guide/src/chapter01/Test01.java
@@ -0,0 +1,12 @@
+package chapter01;
+
+import org.testng.annotations.Test;
+
+public class Test01 {
+
+	@Test
+	public void testMethod() {
+		System.out.println("chapter01 Test01#testMethod");
+	}
+	// Right click -> Run As -> TestNG Test
+}
