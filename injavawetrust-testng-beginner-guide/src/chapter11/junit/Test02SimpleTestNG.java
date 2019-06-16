b2cce72958215aaabe19ee9ef860e743d4309ab9
leventerguder <erguder.levent@gmail.com>
2019-06-16 14:05:17 +0300

    chapter 11 review

---
+++ injavawetrust-testng-beginner-guide/src/chapter11/junit/Test02SimpleTestNG.java
@@ -0,0 +1,24 @@
+package chapter11.junit;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Test02SimpleTestNG {
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Testng before method");
+	}
+
+	@Test
+	public void testMethod() {
+		System.out.println("Testng test method");
+	}
+
+	@AfterMethod
+
+	public void afterMethod() {
+		System.out.println("Testng after method");
+	}
+}
